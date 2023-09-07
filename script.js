7e07c411841afa3103584cffd63514d9cadd81b5
ray-sarthak <117006899+ray-sarthak@users.noreply.github.com>
2023-09-07 15:41:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 function getFormvalue() {
     //Write your code here
+	const name=document.getElementById("form1");
+	 
+	// name.forEach((item) =>allname+item );
+	let fname=document.getElementsByTagName("input")[0].value;
+	let lname=document.getElementsByTagName("input")[1].value;
+	
+	
+	alert(fname+" "+lname);
+	
 
 }
